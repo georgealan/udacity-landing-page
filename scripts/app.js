2f8f47488a1dda35f4273ec4b9ffb9ac6ac3bc5f
George Alan <georgealanrufo@gmail.com>
2023-05-08 18:35:36 -0300

    Design for breeds section defined and almost finished, improved hero slide with animation

--- scripts/app.js
+++ scripts/app.js
@@ -1,141 +1,156 @@
-const menuNavigation = document.getElementById('menu-navigation')
-const sectionsLinks = document.getElementsByClassName('sections')
-const btnScrollToTop = document.getElementById('btn-scroll-top')
-const headerStyle = document.querySelector('.header')
-const navbarStyle = document.querySelector('.nav')
-const videoBackground = document.getElementById('video-bg')
-const heroText = document.getElementById('hero-text')
-const btnSlideLeft = document.getElementById('btn-slide-left')
-const btnSlideRight = document.getElementById('btn-slide-right')
-const heroSection = document.getElementById('hero')
+window.onload = () => {
+    const menuNavigation = document.getElementById('menu-navigation')
+    const sectionsLinks = document.getElementsByClassName('sections')
+    const btnScrollToTop = document.getElementById('btn-scroll-top')
+    const headerStyle = document.querySelector('.header')
+    const navbarStyle = document.querySelector('.nav')
+    const videoBackground = document.getElementById('video-bg')
+    const heroText = document.getElementById('hero-text')
+    const btnSlideLeft = document.getElementById('btn-slide-left')
+    const btnSlideRight = document.getElementById('btn-slide-right')
 
 
-// Fill navbar dinamically
-for(const section of sectionsLinks) {
-    const list = document.createElement('li')
-    const link = document.createElement('a')
-    let hrefLink = '#' + section.id
-    
-    link.setAttribute('href', hrefLink)
-    list.classList.add(section.id)
-    link.innerText = section.dataset.nav
-    list.appendChild(link)
-    menuNavigation.appendChild(list)
-}
-
-// Page Scrool Events
-window.addEventListener('scroll', () => {
-    // Control Button Scroll to Top
-    if(document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
-        btnScrollToTop.style.display = 'block'
-    } else {
-        btnScrollToTop.style.display = 'none'
-    }
-
-    // Control Menu Bar Sticky
-    if(document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
-        headerStyle.style.position = 'sticky'
-        headerStyle.style.top = 0
-        headerStyle.style.justifyContent = 'center'
-        headerStyle.style.width = 100 + 'vw'
-        headerStyle.style.backgroundColor = '#ffffff'
-        
-        navbarStyle.style.width = 100 + '%'
-        navbarStyle.style.height = 70 + 'px'
-        navbarStyle.style.backgroundColor = null
-    } else {
-        headerStyle.style.position = 'absolute'
-        headerStyle.style.top = 40 + 'px'
-        headerStyle.style.justifyContent = null
-        headerStyle.style.width = null
-        headerStyle.style.backgroundColor = null
-        
-        navbarStyle.style.width = 85 + 'vw'
-        navbarStyle.style.height = 80 + 'px'
-        navbarStyle.style.backgroundColor = '#ffffff'
-    }
-    
-    // setTimeout(() => {
-    //     console.log('Scrolling has stopped!')
-    //     headerStyle.style.display = 'none'
-    // }, 5000)
-
-    // Control Active Links
-    let scrollPosition = document.documentElement.scrollTop
+    // Fill navbar dinamically
     for(const section of sectionsLinks) {
-        if(scrollPosition >= section.offsetTop - section.offsetHeight * 0.25 && 
-            scrollPosition < section.offsetTop + section.offsetHeight - section.offsetHeight * 0.25) {
-            activeClass(section.attributes.id.value)
+        const list = document.createElement('li')
+        const link = document.createElement('a')
+        let hrefLink = '#' + section.id
+        
+        if(section.id === 'hero') {
+            list.classList.add('active')
         }
-    } 
-    
-})
 
-// document.addEventListener('wheel', (e) => {
-//     let secBread = document.querySelector('.sticky')
-//     secBread.scrollLeft += e.deltaY
-// })
+        link.setAttribute('href', hrefLink)
+        list.classList.add(section.id)
+        link.innerText = section.dataset.nav
+        list.appendChild(link)
+        menuNavigation.appendChild(list)
+    }
 
-function activeClass(id) {
-    const linksNav = document.querySelectorAll('#menu-navigation li')
-    linksNav.forEach(link => {
-        link.classList.remove('active')
-        if(link.classList.contains(id)) {
-            link.classList.add('active')
+    // Page Scrool Events
+    window.addEventListener('scroll', () => {
+        // Control Button Scroll to Top
+        if(document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+            btnScrollToTop.style.display = 'block'
+        } else {
+            btnScrollToTop.style.display = 'none'
         }
+
+        // Control Menu Bar Sticky
+        if(document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+            headerStyle.style.position = 'sticky'
+            headerStyle.style.top = 0
+            headerStyle.style.justifyContent = 'center'
+            headerStyle.style.width = 100 + 'vw'
+            headerStyle.style.backgroundColor = '#ffffff'
+            
+            navbarStyle.style.width = 100 + '%'
+            navbarStyle.style.height = 70 + 'px'
+            navbarStyle.style.backgroundColor = null
+        } else {
+            headerStyle.style.position = 'absolute'
+            headerStyle.style.top = 40 + 'px'
+            headerStyle.style.justifyContent = null
+            headerStyle.style.width = null
+            headerStyle.style.backgroundColor = null
+            
+            navbarStyle.style.width = 85 + 'vw'
+            navbarStyle.style.height = 80 + 'px'
+            navbarStyle.style.backgroundColor = '#ffffff'
+        }
+
+        // Control Active Links
+        let scrollPosition = document.documentElement.scrollTop
+        for(const section of sectionsLinks) {
+            if(scrollPosition >= section.offsetTop - section.offsetHeight * 0.25 && 
+                scrollPosition < section.offsetTop + section.offsetHeight - section.offsetHeight * 0.25) {
+                activeClass(section.attributes.id.value)
+            }
+        } 
+        
     })
-}
 
-// Control video background slider
-let count = 1
-videoBackground.src = 'assets/videos/video-0' + count + '.mp4'
-fillHeroTextContent(count)
-
-// Takes the contents of the json file to fill the html tags in the corresponding section
-function fillHeroTextContent(videoNumber) {
-    fetch('hero-text.json').then((response) => {
-        response.json().then((data) => {
-            heroText.children[0].innerText = data.contents[videoNumber - 1].title
-            heroText.children[1].innerText = data.contents[videoNumber - 1].body
-            heroText.children[2].href = data.contents[videoNumber - 1].link
+    function activeClass(id) {
+        const linksNav = document.querySelectorAll('#menu-navigation li')
+        linksNav.forEach(link => {
+            link.classList.remove('active')
+            if(link.classList.contains(id)) {
+                link.classList.add('active')
+            }
         })
-    })
-}
-
-/*
-It listens when the video ends and after that it switches to the next video in 
-the folder until it finds the last one and starts over in an infinite loop 
- */
-videoBackground.addEventListener('ended', () => {
-    if(count === 5) {
-        count = 0
     }
-    count++
+
+    // Control video background slider
+    function transitionAnimationVideoRight() { // Control animation video slide css clip-path
+        videoBackground.classList.add('animation-right')
+        setTimeout(() => {
+            videoBackground.classList.remove('animation-right')
+        }, 1000)
+    }
+
+    function transitionAnimationVideoLeft() { // Control animation video slide css clip-path
+        videoBackground.classList.add('animation-left')
+        setTimeout(() => {
+            videoBackground.classList.remove('animation-left')
+        }, 1000)
+    }
+
+    // Populates the hero's content for the first time
+    let count = 1
     videoBackground.src = 'assets/videos/video-0' + count + '.mp4'
+    transitionAnimationVideoRight()
     fillHeroTextContent(count)
-    videoBackground.play()
-})
 
-// Manual control to switch the video in descending order
-btnSlideLeft.addEventListener('click', () => {
-    if(count > 1) {
-        count--
-        videoBackground.src = 'assets/videos/video-0' + count + '.mp4'
-        fillHeroTextContent(count)
+
+    // Takes the contents of the json file to fill the html tags in the corresponding section
+    function fillHeroTextContent(videoNumber) {
+        fetch('hero-text.json').then((response) => {
+            response.json().then((data) => {
+                heroText.children[0].innerText = data.contents[videoNumber - 1].title
+                heroText.children[1].innerText = data.contents[videoNumber - 1].body
+                heroText.children[2].href = data.contents[videoNumber - 1].link
+            })
+        })
     }
-})
 
-// Manual control to change the video, in an increasing order
-btnSlideRight.addEventListener('click', () => {
-    if(count < 5) {
+    /*
+    It listens when the video ends and after that it switches to the next video in 
+    the folder until it finds the last one and starts over in an infinite loop 
+    */
+    videoBackground.addEventListener('ended', () => {
+        if(count === 5) {
+            count = 0
+        }
         count++
         videoBackground.src = 'assets/videos/video-0' + count + '.mp4'
         fillHeroTextContent(count)
-    }
-})
+        transitionAnimationVideoRight()
+        videoBackground.play()
+    })
 
-// Control buttom scroll to top
-btnScrollToTop.addEventListener('click', () => {
-    document.body.scrollTop = 0
-    document.documentElement.scrollTop = 0
-})
+    // Manual control to switch the video in descending order
+    btnSlideLeft.addEventListener('click', () => {
+        if(count > 1) {
+            count--
+            videoBackground.src = 'assets/videos/video-0' + count + '.mp4'
+            fillHeroTextContent(count)
+            transitionAnimationVideoLeft()
+        }
+    })
+
+    // Manual control to change the video, in an increasing order
+    btnSlideRight.addEventListener('click', () => {
+        if(count < 5) {
+            count++
+            videoBackground.src = 'assets/videos/video-0' + count + '.mp4'
+            fillHeroTextContent(count)
+            transitionAnimationVideoRight()
+        }
+    })
+
+    // Control buttom scroll to top
+    btnScrollToTop.addEventListener('click', () => {
+        document.body.scrollTop = 0
+        document.documentElement.scrollTop = 0
+    })
+}
